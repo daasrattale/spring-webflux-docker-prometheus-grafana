69ce2fbd792b45a484b5bc57d2a41104abaa8d9f
saadelattar <sa.elattar.ad@gmail.com>
2023-09-10 18:11:05 +0100

    setting up dependencies.

---
+++ src/main/java/io/daasrattale/springwebluxgrafanaprometheus/SpringWebluxGrafanaPrometheusApplication.java
@@ -0,0 +1,13 @@
+package io.daasrattale.springwebluxgrafanaprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebluxGrafanaPrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebluxGrafanaPrometheusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/daasrattale/springwebluxgrafanaprometheus/SpringWebluxGrafanaPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.daasrattale.springwebluxgrafanaprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebluxGrafanaPrometheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
